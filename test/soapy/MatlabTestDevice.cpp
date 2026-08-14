fc0b4f87931258a13cc631bb00a81409f903a9ce
George Mencoff <george.mencoff@gmail.com>
2026-08-14 16:05:38 -0400

    use C++ function sigs

--- test/soapy/MatlabTestDevice.cpp
+++ test/soapy/MatlabTestDevice.cpp
@@ -1,29 +1,30 @@
-#include <SoapySDR/Device.h>
-#include <SoapySDR/Registry.h>
+#include <SoapySDR/Device.hpp>
+#include <SoapySDR/Registry.hpp>
 
 #include <string>
 #include <vector>
 
-static std::vector<SoapySDRKwargs> findMatlabTestDevices(
-        const SoapySDRKwargs*) {
-    std::vector<SoapySDRKwargs> results;
+static std::vector<SoapySDR::Kwargs> findMatlabTestDevices(
+        const SoapySDR::Kwargs&) {
+    std::vector<SoapySDR::Kwargs> results;
 
-    SoapySDRKwargs dev1 = {};
-    SoapySDRKwargs_set(&dev1, "driver", "matlab_test");
-    SoapySDRKwargs_set(&dev1, "label", "MATLAB Test Device 1");
-    SoapySDRKwargs_set(&dev1, "serial", "TEST001");
+    SoapySDR::Kwargs dev1;
+    dev1["driver"] = "matlab_test";
+    dev1["label"] = "MATLAB Test Device 1";
+    dev1["serial"] = "TEST001";
     results.push_back(dev1);
 
-    SoapySDRKwargs dev2 = {};
-    SoapySDRKwargs_set(&dev2, "driver", "matlab_test");
-    SoapySDRKwargs_set(&dev2, "label", "MATLAB Test Device 2");
-    SoapySDRKwargs_set(&dev2, "serial", "TEST002");
+    SoapySDR::Kwargs dev2;
+    dev2["driver"] = "matlab_test";
+    dev2["label"] = "MATLAB Test Device 2";
+    dev2["serial"] = "TEST002";
     results.push_back(dev2);
 
     return results;
 }
 
-static SoapySDRDevice* makeMatlabTestDevice(const SoapySDRKwargs*) {
+static SoapySDR::Device* makeMatlabTestDevice(
+        const SoapySDR::Kwargs&) {
     return nullptr;
 }
 
